adc4549144c034d63b52677908ffd12bc9402d02
ToanNguyen9602 <toannguyen9602@gmail.com>
2024-07-12 17:14:06 +0700

    commit 12/07

--- BackEnd/shopapp/src/main/java/com/project/shopapp/filters/JwtTokenFilter.java
+++ BackEnd/shopapp/src/main/java/com/project/shopapp/filters/JwtTokenFilter.java
@@ -1,6 +1,7 @@
 package com.project.shopapp.filters;
 
 import com.project.shopapp.components.JwtTokenUtil;
+import com.project.shopapp.models.User;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,8 +10,12 @@ import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.util.Pair;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -32,18 +37,36 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
         //cho đi qua hết - enable bypass
 //        filterChain.doFilter(request,response);
-        if (isByPassToken(request)) {
-            filterChain.doFilter(request, response);
-            return;
+        //request không yêu cầuToken
+        try {
+            if (isByPassToken(request)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            //request yêu cầu Token
+            final String authHeader = request.getHeader("Authorization");
+            if (authHeader != null && authHeader.startsWith("Bearer ")
+            ) {
+                //bỏ 7 ký tự đầu tiên sẽ được token
+                final String token = authHeader.substring(7);
+                final String phoneNumber = jwtTokenUtil.extractPhoneNumber(token);
+                if (phoneNumber != null &&
+                        SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(phoneNumber);
+                    if (jwtTokenUtil.validateToken(token, userDetails)) {
+                        UsernamePasswordAuthenticationToken authenticationToken =
+                                new UsernamePasswordAuthenticationToken(
+                                        userDetails, null, userDetails.getAuthorities()
+                                );
+                        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    }
+                }
+            }
+            filterChain.doFilter(request,response);
+        } catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
         }
-        final String authHeader = request.getHeader("Authorization");
-        if (authHeader != null && authHeader.startsWith("Bearer ")
-        ) {
-            //bỏ 7 ký tự đầu tiên sẽ được token
-            final String token = authHeader.substring(7);
-            jwtTokenUtil.extractPhoneNumber(token);
-        }
-
     }
 
     private boolean isByPassToken(@NotNull HttpServletRequest request) {
