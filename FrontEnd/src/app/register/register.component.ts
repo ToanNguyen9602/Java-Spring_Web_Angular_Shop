b72a907f262efcc6eb6daae156f504e812628f5e
ToanNguyen9602 <toannguyen9602@gmail.com>
2024-07-17 18:02:33 +0700

    commit validate_register_end

--- FrontEnd/src/app/register/register.component.ts
+++ FrontEnd/src/app/register/register.component.ts
@@ -7,7 +7,7 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent {
-  @ViewChild('registerForm') registerForm!: NgForm;
+  @ViewChild("registerForm") registerForm!: NgForm;
   //Khai báo các biến tương ứng trường dữ liệu trên form
   phone: string;
   password: string;
@@ -44,9 +44,29 @@ export class RegisterComponent {
   }
   checkPasswordsMatch() {
     if (this.password !== this.retypePassword) {
-    this.registerForm.form.controls['retypePassword'].setErrors({'passwordMismatch': true});
+      this.registerForm.form.controls["retypePassword"].setErrors({
+        passwordMismatch: true,
+      });
     } else {
-      this.registerForm.form.controls['retypePassword'].setErrors(null);
+      this.registerForm.form.controls["retypePassword"].setErrors(null);
+    }
+  }
+  checkAge() {
+    if (this.dateOfBirth) {
+      const today = new Date();
+      const birthDate = new Date(this.dateOfBirth);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+      
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      if (age < 18) {
+        this.registerForm.form.controls['dateOfBirth'].setErrors({'invalidAge':true});
+      } else {
+        this.registerForm.form.controls['dateOfBirth'].setErrors(null);
+      }
     }
   }
 }
