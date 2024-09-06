def795ff4c7e820e375e68b65087e8c4eceeb582
ToanNguyen9602 <toannguyen9602@gmail.com>
2024-09-06 14:34:47 +0700

    commit  yarn_install_jwt

--- FrontEnd/src/app/service/token.service.ts
+++ FrontEnd/src/app/service/token.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@angular/core";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: "root",
 })
 export class TokenService {
   private readonly TOKEN_KEY = "access_token";
+  private jwtHelperService = new JwtHelperService();
   constructor() {}
   //getter
   getToken(): string | null {
@@ -16,5 +18,17 @@ export class TokenService {
   }
   removeToken(): void {
     localStorage.removeItem(this.TOKEN_KEY);
+  }
+  getUserId(): number {
+    //lấy ra token
+    let userObject = this.jwtHelperService.decodeToken(this.getToken() ?? "");
+    //kiểm tra trong token có trường userId trong claims hay k
+    return "userId" in userObject ? parseInt(userObject["userId"]) : 0;
+  }
+  isTokenExpired(): boolean {
+    if (this.getToken() == null) {
+      return false;
+    }
+    return this.jwtHelperService.isTokenExpired(this.getToken()!);
   }
 }
